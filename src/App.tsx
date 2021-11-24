06aa6a2a1066925ea935643fc5786448fb9b9e97
Bardob1214 <doboszb14@gmail.com>
2021-11-24 14:24:55 +0100

    Add moveable items

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,30 @@ import React, { useState } from 'react';
 import { v4 as uuid } from 'uuid';
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
+const backendItems = [
+    { id: uuid(), content: "Zadanie 1" },
+    { id: uuid(), content: "Zadanie 2" }
+];
+
 const backendColumns = {
     [uuid()]: {
         name: "Do zrobienia",
+        items: backendItems
     },
     [uuid()]: {
         name: "W trakcie",
+        items: []
     },
     [uuid()]: {
         name: "Zrobione",
+        items: []
     }
 };
 
 function App() {
     const [columns, setColumns] = useState(backendColumns);
     return (
-        <div style={{display:"flex", justifyContent:"center", height:"100vh"}}>
+        <div style={{ display: "flex", justifyContent: "center", height: "100vh" }}>
             <DragDropContext onDragEnd={result => console.log(result)}>
                 {Object.entries(columns).map(([columnId, column]) => {
                     return (
@@ -31,12 +39,36 @@ function App() {
                                                 {...provided.droppableProps}
                                                 ref={provided.innerRef}
                                                 style={{
-                                                    background: snapshot.isDraggingOver ? "grey" : "lightgrey",
+                                                    background: snapshot.isDraggingOver ? "#808080" : "#d3d3d3",
                                                     padding: 4,
                                                     width: 300,
                                                     minHeight: 700
                                                 }}
                                             >
+                                                {column.items.map((item, index) => {
+                                                    return (
+                                                        <Draggable key={item.id} draggableId={item.id} index={index}>
+                                                            {(provided, snapshot) => {
+                                                                return (
+                                                                    <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}
+                                                                        style={{
+                                                                            userSelect: "none",
+                                                                            margin: "5px 5px 10px 5px",
+                                                                            minHeight: "35px",
+                                                                            padding: 10,
+                                                                            color: "#FFFAFA",
+                                                                            backgroundColor: snapshot.isDragging ? "#264a2b" : "#478645",
+                                                                            ...provided.draggableProps.style
+                                                                        }}
+                                                                    >
+                                                                        {item.content}
+                                                                    </div>
+                                                                )
+                                                            }}
+                                                        </Draggable>
+                                                    )
+                                                })
+                                                }
                                             </div>
                                         )
                                     }}
