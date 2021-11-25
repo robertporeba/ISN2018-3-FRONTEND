bd49e7ffd5532871c2ad57e7016f7362dfeea7ea
Bardob1214 <doboszb14@gmail.com>
2021-11-25 15:24:06 +0100

    Add dropping movable items in different columns

--- src/App.tsx
+++ src/App.tsx
@@ -22,11 +22,48 @@ const backendColumns = {
     }
 };
 
+const onDragEnd = (result: any, columns: any, setColumns: any) => {
+    if (!result.destination) return;
+    const { source, destination } = result;
+
+    if (source.droppableId !== destination.droppableId) {
+        const sourceColumn = columns[source.droppableId];
+        const destColumn = columns[destination.droppableId];
+        const sourceItems = [...sourceColumn.items];
+        const destItems = [...destColumn.items];
+        const [removed] = sourceItems.splice(source.index, 1);
+        destItems.splice(destination.index, 0, removed);
+        setColumns({
+            ...columns,
+            [source.droppableId]: {
+                ...sourceColumn,
+                items: sourceItems
+            },
+            [destination.droppableId]: {
+                ...destColumn,
+                items: destItems
+            }
+        });
+    } else {
+        const column = columns[source.droppableId];
+        const copiedItems = [...column.items];
+        const [removed] = copiedItems.splice(source.index, 1);
+        copiedItems.splice(destination.index, 0, removed);
+        setColumns({
+            ...columns,
+            [source.droppableId]: {
+                ...column,
+                items: copiedItems
+            }
+        });
+    }
+};
+
 function App() {
     const [columns, setColumns] = useState(backendColumns);
     return (
         <div style={{ display: "flex", justifyContent: "center", height: "100vh" }}>
-            <DragDropContext onDragEnd={result => console.log(result)}>
+            <DragDropContext onDragEnd={result => onDragEnd(result, columns, setColumns)}>
                 {Object.entries(columns).map(([columnId, column]) => {
                     return (
                         <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }} key={columnId}>
@@ -69,6 +106,7 @@ function App() {
                                                     )
                                                 })
                                                 }
+                                                {provided.placeholder}
                                             </div>
                                         )
                                     }}
