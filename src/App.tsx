3d20290db4da49c24bf8b47cf78fdcce04e3fbfe
Robert Poręba <webberr14@gmail.com>
2021-11-28 18:54:05 +0100

    28.11.21 #routing with role secure access

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import Home from './pages/home/Home';
+import React, { lazy, Suspense } from 'react';
+import { Router, Route } from 'react-router-dom';
+import { history } from './utils/history';
+import { RoleRoute } from './utils/http';
+
+import Loader from './components/util/Loader';
 
 import './App.scss';
+import { Admin, User } from './interfaces/auth';
+
+const Home = lazy(() => import('./pages/home/Home'));
+const UserPage = lazy(() => import('./pages/admin-panel/AdminPanel'));
 
 function App() {
 	return (
 		<div className="App">
-			<Home />
+			<Router history={history}>
+				<Suspense fallback={<Loader />}>
+					<Route exact path="/" component={Home} />
+					<RoleRoute path="/admin-panel" role={[User, Admin]}>
+						<UserPage />
+					</RoleRoute>
+				</Suspense>
+			</Router>
 		</div>
 	);
 }
