2b4c863dcad88f699b74c98bb585bb64b77fc5b8
Bardob1214 <doboszb14@gmail.com>
2021-11-29 19:39:46 +0100

    Small changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { v4 as uuid } from 'uuid';
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import "./App.css";
+import DragEnd from "./components/DragEnd";
 
 const backendItems = [
     { id: uuid(), content: "Zadanie 1" },
@@ -23,42 +24,6 @@ const backendColumns = {
     }
 };
 
-const onDragEnd = (result: any, columns: any, setColumns: any) => {
-    if (!result.destination) return;
-    const { source, destination } = result;
-
-    if (source.droppableId !== destination.droppableId) {
-        const sourceColumn = columns[source.droppableId];
-        const destColumn = columns[destination.droppableId];
-        const sourceItems = [...sourceColumn.items];
-        const destItems = [...destColumn.items];
-        const [removed] = sourceItems.splice(source.index, 1);
-        destItems.splice(destination.index, 0, removed);
-        setColumns({
-            ...columns,
-            [source.droppableId]: {
-                ...sourceColumn,
-                items: sourceItems
-            },
-            [destination.droppableId]: {
-                ...destColumn,
-                items: destItems
-            }
-        });
-    } else {
-        const column = columns[source.droppableId];
-        const copiedItems = [...column.items];
-        const [removed] = copiedItems.splice(source.index, 1);
-        copiedItems.splice(destination.index, 0, removed);
-        setColumns({
-            ...columns,
-            [source.droppableId]: {
-                ...column,
-                items: copiedItems
-            }
-        });
-    }
-};
 
 function App() {
     const [columns, setColumns] = useState(backendColumns);
@@ -81,7 +46,7 @@ function App() {
                         </div>
                 </div>
             </div>
-            <DragDropContext onDragEnd={result => onDragEnd(result, columns, setColumns)}>
+            <DragDropContext onDragEnd={result => DragEnd(result, columns, setColumns)}>
                 {Object.entries(columns).map(([columnId, column]) => {
                     return (
                         <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }} key={columnId}>
