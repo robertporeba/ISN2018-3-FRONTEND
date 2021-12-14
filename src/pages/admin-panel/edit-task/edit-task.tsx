5c458c2c1e3d86df5a076b7f9cf1dbd85df2f7df
Robert Poręba <webberr14@gmail.com>
2021-12-14 20:07:48 +0100

    14.12.21 #update task and tasks status

--- src/pages/admin-panel/edit-task/edit-task.tsx
+++ src/pages/admin-panel/edit-task/edit-task.tsx
@@ -22,6 +22,9 @@ function EditTask() {
 	const [panelForm, setPanelForm] = useState<boolean>(true);
 	const [invalidData, setInvalidData] = useState(false);
 	const [tasks, setTasks] = useState<any>();
+	const [name, setName] = useState<any>();
+	const [assignedUser, setAssignedUser] = useState<any>();
+	const [priority, setPriority] = useState<any>();
 	const { id } = useParams<IEditParam>();
 	let taskId = parseInt(id);
 	const getTask = useCallback(() => {
@@ -30,7 +33,6 @@ function EditTask() {
 			.then((response) => {
 				setTasks(response);
 				console.log(response);
-			
 			})
 			.catch((err) => {
 				console.log(err);
@@ -41,58 +43,72 @@ function EditTask() {
 		getTask();
 	}, [getTask]);
 
+	const updateTask = () => {
+		taskService
+			.updatetask({
+				id: id,
+				name: name,
+				assignedUser: assignedUser,
+				priorityId: priority,
+			})
+			.then((response) => {
+				console.log(response);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
 
 	return (
-
 		<div className="admin-panel-container">
 			{tasks && (
 				<>
-		<HeaderPanel setPanelForm={setPanelForm} />
-		
-		
-		<div className="login-container">
-		
-			<h1 className="login-container__title">Edycja Taska</h1>
-			<form className="login-container__form">
-		
-				<label className="login-container__form__email">Nazwa</label>
-				<br />
-				
-				<input type="text" value={tasks.name} />
-				<br />
-				<label className="login-container__form__password">Osoba przypisana</label>
-				<br />
-				<input type="text" value={tasks.assignedUser} />
-				<br />
-				<label className="login-container__form__password">Priorytet</label>
-				<br />
-				<select value={tasks.priorityName}>
-					<option value="LOW">LOW</option>
-					<option value="MEDIUM">MEDIUM</option>
-					<option value="HIGH">HIGH</option>
-				</select>
-				
-				 	<br />
-				
-					 <input
-					
-					className="login-container__form__button"
-					type="submit"
-					value="Edytuj"
-				/>
-				
-			
-			</form>
-			
-		</div>
-	</>
-	)}
+					<HeaderPanel setPanelForm={setPanelForm} />
 
+					<div className="login-container">
+						<h1 className="login-container__title">Edycja Taska</h1>
+						<form className="login-container__form">
+							<label className="login-container__form__email">Nazwa</label>
+							<br />
+
+							<input
+								type="text"
+								onChange={(e) => setName(e.target.value)}
+								value={name}
+							/>
+							<br />
+							<label className="login-container__form__password">
+								Osoba przypisana
+							</label>
+							<br />
+							<input
+								type="text"
+								value={assignedUser}
+								onChange={(e) => setAssignedUser(e.target.value)}
+							/>
+							<br />
+							<label className="login-container__form__password">Priorytet</label>
+							<br />
+							<select value={priority} onChange={(e) => setPriority(e.target.value)}>
+								<option value="1">LOW</option>
+								<option value="2">MEDIUM</option>
+								<option value="3">HIGH</option>
+							</select>
+
+							<br />
+
+							<input
+								onClick={() => updateTask()}
+								className="login-container__form__button"
+								type="button"
+								value="Edytuj"
+							/>
+						</form>
+					</div>
+				</>
+			)}
 		</div>
-		
 	);
 }
 
 export default EditTask;
-
-
